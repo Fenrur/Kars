7f01ea9c1a048c13310aa93ecd0c42bf19be3e2e
Livio Tinnirello <tinnirellolivio@gmail.com>
2024-12-10 13:54:28 +0100

    chore: init project

--- release-version-plugin.js
+++ release-version-plugin.js
@@ -5,13 +5,13 @@ async function prepare(pluginConfig, context) {
 
     await writeFile('./VERSION', version, 'utf8');
 
-    const contentGradleProperties = await readFile('./template-backend/gradle.properties', 'utf8')
+    const contentGradleProperties = await readFile('./kars-backend/gradle.properties', 'utf8')
     const newContentGradleProperties = contentGradleProperties.replace(/projectVersion=.*/, `projectVersion=${version}`);
-    await writeFile('./template-backend/gradle.properties', newContentGradleProperties, 'utf8');
+    await writeFile('./kars-backend/gradle.properties', newContentGradleProperties, 'utf8');
     
-    const contentPackageJson = await readFile('./template-frontend/package.json', 'utf8')
+    const contentPackageJson = await readFile('./kars-frontend/package.json', 'utf8')
     const newContentPackageJson = contentPackageJson.replace(/"version": ".*/, `"version": "${version}",`);
-    await writeFile('./template-frontend/package.json', newContentPackageJson, 'utf8');
+    await writeFile('./kars-frontend/package.json', newContentPackageJson, 'utf8');
 }
 
 module.exports = { prepare };
