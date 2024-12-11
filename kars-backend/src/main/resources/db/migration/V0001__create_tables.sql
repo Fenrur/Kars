4ce6de7fd51e94fb22d0100a26ff6a07d9394d67
Livio Tinnirello <tinnirellolivio@gmail.com>
2024-12-11 17:10:40 +0100

    chore: Add docs and sql

--- kars-backend/src/main/resources/db/migration/V0001__create_tables.sql
+++ kars-backend/src/main/resources/db/migration/V0001__create_tables.sql
@@ -1,15 +1,132 @@
-CREATE TABLE authors
+CREATE TABLE city
 (
-    id         SERIAL PRIMARY KEY,
-    last_name  TEXT NOT NULL ,
-    first_name TEXT NOT NULL 
+    city_id                    SERIAL,
+    department            VARCHAR(3)   DEFAULT NULL,
+    slug                  VARCHAR(255) DEFAULT NULL UNIQUE,
+    name                  VARCHAR(45)  DEFAULT NULL,
+    simple_name           VARCHAR(45)  DEFAULT NULL,
+    official_name         VARCHAR(45)  DEFAULT NULL,
+    soundex_name          VARCHAR(20)  DEFAULT NULL,
+    metaphone_name        VARCHAR(22)  DEFAULT NULL,
+    postal_code           VARCHAR(255) DEFAULT NULL,
+    commune               VARCHAR(3)   DEFAULT NULL,
+    commune_code          VARCHAR(5) NOT NULL UNIQUE,
+    arrondissement        SMALLINT     DEFAULT NULL,
+    canton                VARCHAR(4)   DEFAULT NULL,
+    amdi                  INTEGER      DEFAULT NULL,
+    population_2010       INTEGER      DEFAULT NULL,
+    population_1999       INTEGER      DEFAULT NULL,
+    population_2012       INTEGER      DEFAULT NULL,
+    density_2010          INTEGER      DEFAULT NULL,
+    area                  REAL         DEFAULT NULL,
+    longitude_deg         REAL         DEFAULT NULL,
+    latitude_deg          REAL         DEFAULT NULL,
+    longitude_grd         VARCHAR(9)   DEFAULT NULL,
+    latitude_grd          VARCHAR(8)   DEFAULT NULL,
+    longitude_dms         VARCHAR(9)   DEFAULT NULL,
+    latitude_dms          VARCHAR(8)   DEFAULT NULL,
+    min_altitude          INTEGER      DEFAULT NULL,
+    max_altitude          INTEGER      DEFAULT NULL,
+    PRIMARY KEY (city_id),
+    UNIQUE (slug)
 );
 
-CREATE TABLE books
+CREATE TABLE party_subtype
 (
-    id             SERIAL PRIMARY KEY,
-    title          TEXT NOT NULL ,
-    description    TEXT NOT NULL ,
-    published_year INT NOT NULL ,
-    author_id      INT NOT NULL REFERENCES authors (id)
+    party_subtype_id SERIAL,
+    name             VARCHAR(250) NOT NULL,
+    type             VARCHAR(50)  NOT NULL,
+    PRIMARY KEY (party_subtype_id)
+);
+
+CREATE TABLE userr
+(
+    user_id SERIAL,
+    name    VARCHAR(100) NOT NULL,
+    email   VARCHAR(100) NOT NULL,
+    PRIMARY KEY (user_id),
+    UNIQUE (email)
+);
+
+CREATE TABLE profile
+(
+    profile_id SERIAL,
+    born_at    DATE        NOT NULL,
+    gender     VARCHAR(50) NOT NULL,
+    user_id    INTEGER     NOT NULL,
+    city_id    INTEGER     NOT NULL,
+    PRIMARY KEY (profile_id),
+    UNIQUE (user_id),
+    FOREIGN KEY (user_id) REFERENCES userr (user_id),
+    FOREIGN KEY (city_id) REFERENCES city (city_id)
+);
+
+CREATE TABLE party
+(
+    party_id         SERIAL,
+    type             VARCHAR(50)                    NOT NULL,
+    price            DOUBLE PRECISION,
+    name             VARCHAR(250)                   NOT NULL,
+    start_at         TIMESTAMP(3) WITHOUT TIME ZONE NOT NULL,
+    end_at           TIMESTAMP(3) WITHOUT TIME ZONE,
+    maximum_place    INTEGER                        NOT NULL,
+    city_id          INTEGER                        NOT NULL,
+    owner_profile_id INTEGER                        NOT NULL,
+    PRIMARY KEY (party_id),
+    FOREIGN KEY (city_id) REFERENCES city (city_id),
+    FOREIGN KEY (owner_profile_id) REFERENCES profile (profile_id)
+);
+
+CREATE TABLE party_subtype_list
+(
+    id               SERIAL,
+    party_subtype_id INTEGER NOT NULL,
+    party_id         INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (party_subtype_id) REFERENCES party_subtype (party_subtype_id),
+    FOREIGN KEY (party_id) REFERENCES party (party_id)
+);
+
+CREATE TABLE evaluate_profile_party
+(
+    evaluated_profile_id INTEGER,
+    evaluator_profile_id INTEGER,
+    party_id             INTEGER,
+    rate                 SMALLINT NOT NULL,
+    comment              VARCHAR(1000),
+    PRIMARY KEY (evaluated_profile_id, evaluator_profile_id, party_id),
+    FOREIGN KEY (evaluated_profile_id) REFERENCES profile (profile_id),
+    FOREIGN KEY (evaluator_profile_id) REFERENCES profile (profile_id),
+    FOREIGN KEY (party_id) REFERENCES party (party_id)
+);
+
+CREATE TABLE attend_party
+(
+    attend_profile_id INTEGER,
+    party_id          INTEGER,
+    PRIMARY KEY (attend_profile_id, party_id),
+    FOREIGN KEY (attend_profile_id) REFERENCES profile (profile_id),
+    FOREIGN KEY (party_id) REFERENCES party (party_id)
+);
+
+CREATE TABLE request_party
+(
+    profile_id INTEGER,
+    party_id   INTEGER,
+    comment    VARCHAR(250),
+    request_at TIMESTAMP(3) WITHOUT TIME ZONE NOT NULL,
+    PRIMARY KEY (profile_id, party_id),
+    FOREIGN KEY (profile_id) REFERENCES profile (profile_id),
+    FOREIGN KEY (party_id) REFERENCES party (party_id)
+);
+
+CREATE TABLE invitation_party
+(
+    profile_id    INTEGER,
+    party_id      INTEGER,
+    comment       VARCHAR(250),
+    invitation_at TIMESTAMP(3) WITHOUT TIME ZONE NOT NULL,
+    PRIMARY KEY (profile_id, party_id),
+    FOREIGN KEY (profile_id) REFERENCES profile (profile_id),
+    FOREIGN KEY (party_id) REFERENCES party (party_id)
 );
